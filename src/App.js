abfaf95ebef9aff8706449afe1d99c0a57548cce
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-04-27 15:00:55 -0800

    got array of vehicles rending

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   // alienSize should be a number that starts out as 10
   const [alienSize, setAlienSize] = useState(10);
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
+  const [vehicleParade, setVehicleParade] = useState(['car', 'truck']);
 
   function handleAlienEat() {
     setAlienSize(alienSize + 1);
@@ -41,6 +42,9 @@ function App() {
     setLightColor('green');
   }
 
+  function handleAddVehicle() {
+
+  }
 
 
   return (
@@ -48,7 +52,7 @@ function App() {
       <div className="fight">
         <div className="monster">
           {/* the width of the alien should be ten times whatever the alien size is in state */}
-          <img src="alien.png" width={alienSize} />
+          <img src="alien.png" width={alienSize * 10} />
           <div className='buttons'>
             {/* when you click this button, the alien's size in state should go up by one */}
             <button onClick={handleAlienEat}>Oh no! The alien is gobblin up all the electricity!</button>
@@ -58,7 +62,7 @@ function App() {
         </div>
         <div className="monster">
           {/* the width of the lizard should be ten times whatever the alien size is in state */}
-          <img src="lizard.png" width={lizardSize} />
+          <img src="lizard.png" width={lizardSize * 10} />
           <div className="buttons">
             {/* when you click this button, the lizard's size in state should go up by one */}
             <button onClick={handleLizardEat}>Yegads! The lizard is ramping up to its final form!</button>
@@ -81,7 +85,7 @@ function App() {
       This prop should be an array of strings like ['car', 'truck', 'truck', 'car', 'bus'].
       Do you have something like that in state that you could pass as a vehicles prop? 
       */}
-      <VehicleList />
+      <VehicleList vehicles={vehicleParade} />
       <div className='buttons'>
         {/* This part is weird */}
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'car' to the end */}
