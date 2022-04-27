6628d9322d6e9fc32b2292b6233a3585c5a07878
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-04-27 14:45:54 -0800

    monster fight working

--- src/App.js
+++ src/App.js
@@ -10,8 +10,26 @@ function App() {
   // lightColor should be a string that starts out as 'red'
   const [lightColor, setLightColor] = useState('red');
   // lizardSize should be a number that starts out as 10
+  const [lizardSize, setLizardSize] = useState(10);
   // alienSize should be a number that starts out as 10
+  const [alienSize, setAlienSize] = useState(10);
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
+
+  function handleAlienEat() {
+    setAlienSize(alienSize + 1);
+  }
+  function handleAlienAttack() {
+    setLizardSize(lizardSize - 1);
+  }
+
+  function handleLizardEat() {
+    setLizardSize(lizardSize + 1);
+  }
+  function handleLizardAttack() {
+    setAlienSize(alienSize - 1);
+  }
+
+
 
   function handleLightRed() {
     setLightColor('red');
@@ -30,22 +48,22 @@ function App() {
       <div className="fight">
         <div className="monster">
           {/* the width of the alien should be ten times whatever the alien size is in state */}
-          <img src="alien.png" width={20} />
+          <img src="alien.png" width={alienSize} />
           <div className='buttons'>
             {/* when you click this button, the alien's size in state should go up by one */}
-            <button>Oh no! The alien is gobblin up all the electricity!</button>
+            <button onClick={handleAlienEat}>Oh no! The alien is gobblin up all the electricity!</button>
             {/* when you click this button, the lizard's size in state should go down by one */}
-            <button >Amazing! The alien zapped the lizard!</button>
+            <button onClick={handleAlienAttack}>Amazing! The alien zapped the lizard!</button>
           </div>
         </div>
         <div className="monster">
           {/* the width of the lizard should be ten times whatever the alien size is in state */}
-          <img src="lizard.png" width={20} />
+          <img src="lizard.png" width={lizardSize} />
           <div className="buttons">
             {/* when you click this button, the lizard's size in state should go up by one */}
-            <button>Yegads! The lizard is ramping up to its final form!</button>
+            <button onClick={handleLizardEat}>Yegads! The lizard is ramping up to its final form!</button>
             {/* when you click this button, the alien's size in state should go up by one */}
-            <button>Oh my! The lizard chomped down on the alien!</button>
+            <button onClick={handleLizardAttack}>Oh my! The lizard chomped down on the alien!</button>
           </div>
         </div>
       </div>
